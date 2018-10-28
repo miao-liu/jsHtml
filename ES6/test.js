42f59ddaec28c955a4c2a76a982c9fe482d99b33
mo <moyiliang@foxmail.com>
2018-10-28 23:34:03 +0800

    first

---
+++ ES6/test.js
@@ -0,0 +1,13 @@
+
+//Array
+let arr=[1,2,3];
+//for(let val in arr)//打印坐标 index
+for(let val of arr)//打印值	value
+console.log(val)
+
+//Object
+let person={name:'monica',age:21};
+
+for(let val in person)//property   
+//for(let val of person)//error
+	console.log(val)
